0664e552f2f12195271d1e7314bc744443752ecb
Svetlana Dyakova <sdyakova@outlook.com>
2024-09-10 17:22:25 +0300

    create Status type

--- src/components/TodoList_v3/components/TodoCreator/index.tsx
+++ src/components/TodoList_v3/components/TodoCreator/index.tsx
@@ -2,16 +2,13 @@ import React, { useState } from "react";
 
 import styles from "./TodoCreator.module.scss";
 import { addNewTodo } from "../../api/addNewTodo";
-import { Todo } from "../../types";
+import { Status, Todo } from "../../types";
 import Spinner from "../../shared/Spinner";
-import useStatus from "../../shared/Hook/useState";
-
-// type Status = "idle" | "loading" | "success" | "error";
 
 const TodoCreator: React.FC<{
   onCreate: React.Dispatch<React.SetStateAction<Todo[]>>;
 }> = ({ onCreate }) => {
-  const [status, setStatus] = useStatus("idle");
+  const [status, setStatus] = useState<Status>("idle");
 
   const handleKeyUpWhenCreate = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const inputValue = e.currentTarget.value.trim();
